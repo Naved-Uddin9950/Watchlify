e224abafc30dadc05bf9b069cf8f7667ba0f9a23
Naved Uddin <naveduddin83@gmail.com>
2024-04-06 09:21:00 +0530

    configured dotenv

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,14 +1,19 @@
+import dotenv from 'dotenv';
 import { DB_NAME } from "../constants.js";
 import mongoose from 'mongoose';
 
-const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017';
+dotenv.config({
+    path: './.env'
+})
+
+const DB_URI = process.env.DB_URI;
 
 const connectDB = async () => {
     try {
-        mongoose.connect(`${DB_URI}/${DB_NAME}`);
+        await mongoose.connect(`${DB_URI}/${DB_NAME}`);
         console.log('Database connected !!!');
     } catch (error) {
-        console.error("\n MongoDB Connection Error : ", error);
+        console.error(`\n MongoDB Connection Error : ${error}`);
         process.exit(1);
     }
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,20 @@
+import dotenv from 'dotenv';
 import connectDB from "./db/connection.js";
+import express from 'express';
 
-connectDB();
+dotenv.config({
+    path: './.env'
+})
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+connectDB()
+.then( () => {
+    app.listen(port, () => {
+        console.log(`Server is connected at port : ${port}`);
+    })
+})
+.catch( (error) => {
+    console.error(`Database connection error : ${error}`);
+})
