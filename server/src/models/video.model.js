dd8ba14a2198e2bbed060199291a97ed96b8c53a
Naved Uddin <naveduddin83@gmail.com>
2024-04-08 16:12:58 +0530

    created video model

--- server/src/models/video.model.js
+++ server/src/models/video.model.js
@@ -1,7 +1,35 @@
 import mongoose from 'mongoose';
 
 const videoSchema = new mongoose.Schema({
-    
+    videoFile : {
+        type : String
+    },
+    thumbnail : {
+        type : String
+    },
+    owner : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User'
+    },
+    title : {
+        type : String,
+        required : [true, 'Title is required']
+    },
+    description : {
+        type : String,
+    },
+    duration : {
+        type : number
+    },
+    views : {
+        type : number,
+        required : [true, 'Views is required'],
+        default : 0
+    },
+    isPublished : {
+        type : Boolean,
+        default : false
+    }
 }, {timestamps: true});
 
 export const Video = mongoose.model('Video', videoSchema);
